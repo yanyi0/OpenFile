255e9d56fe842c5fcea9d34ee2d58369feee0ada
yanyi0 <785144130@qq.com>
2018-11-25 12:22:00 +0800

    Initial Commit

---
+++ OpenFile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenFile
+//
+//  Created by Cloud on 2018/11/25.
+//  Copyright © 2018 Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenFile/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OpenFile
+//
+//  Created by Cloud on 2018/11/25.
+//  Copyright © 2018 Cloud. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
